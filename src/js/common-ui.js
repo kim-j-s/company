5bddf7ab4772cb7f7caed06695562d516ca07e6a
kjs <kogwing@gmail.com>
2024-04-11 16:33:30 +0900

    진행 중

--- src/js/common-ui.js
+++ src/js/common-ui.js
@@ -1,3 +1,21 @@
 import '../css/common.scss'
 
-console.log("?");
+function slide() {
+  $('.slide-act').on('click', function(){
+    var data = $(this).data('slide');
+    var $trg = $("[data-slidecont='" + data + "']");
+
+    if ( $trg.css('display') == 'block' ){
+      $(this).addClass('on');
+      $trg.stop().slideUp(500);
+    } else {
+      $(this).removeClass('on');
+      $trg.stop().slideDown(500);
+    }
+  })
+}
+
+$(function(){
+  // slide
+  slide();
+})
