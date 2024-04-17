207ce6c57d6f71653da44c55f92b1cb7b8502eb0
kjs <kogwing@gmail.com>
2024-04-17 17:13:40 +0900

    gsap

--- src/js/common-ui.js
+++ src/js/common-ui.js
@@ -26,19 +26,49 @@ gsap.to('.gsap_02_img01', {
   duration: 2,
   scrollTrigger: {
     trigger: '.gsap_02_img01',
-    // start: 'top 600px',
     start: 'top 600px',
     end: 'top 100px',
-    // toggleActions: 'restart pause reverse pause',
     // scrub: true,
     scrub: 1,
-    // markers: {
-    //   startColor: 'blue',
-    //   endColor: 'red',
-    //   fontSize: '50px',
-    //   indent: 200
-    // }
-    markers: true
+    // markers: true
+  }
+})
+gsap.to('.gsap_02_img02', {
+  opacity: 1,
+  y: -140,
+  duration: 2,
+  scrollTrigger: {
+    trigger: '.gsap_02_img02',
+    start: 'top 650px',
+    end: 'top 100px',
+    // scrub: true,
+    scrub: 1,
+    // markers: true
+  }
+})
+gsap.to('.gsap_02_img03', {
+  opacity: 1,
+  x: -200,
+  duration: 2,
+  scrollTrigger: {
+    trigger: '.gsap_02',
+    start: 'top 600px',
+    end: 'top 100px',
+    // scrub: true,
+    scrub: 1,
+    // markers: true
   }
 })
 
+gsap.to('.gasp_03 .text', {
+  backgroundPositionX: "0%",
+  stagger: 20,
+  duration: 20,
+  scrollTrigger: {
+    trigger: '.gasp_03 .text',
+    start: 'top 80%',
+    end: 'bottom 25%',
+    scrub: 1,
+    // markers: true
+  }
+})
