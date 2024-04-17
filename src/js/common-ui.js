f1df4514feb3578c0fd51cb1e2663df9c589572d
kjs <kogwing@gmail.com>
2024-04-17 15:24:02 +0900

    gsap

--- src/js/common-ui.js
+++ src/js/common-ui.js
@@ -17,8 +17,28 @@ function slide() {
 $(function(){
   // slide
   slide();
+})
 
-
-
+gsap.registerPlugin(ScrollTrigger);
+gsap.to('.gsap_02_img01', {
+  opacity: 1,
+  y: 200,
+  duration: 2,
+  scrollTrigger: {
+    trigger: '.gsap_02_img01',
+    // start: 'top 600px',
+    start: 'top 600px',
+    end: 'top 100px',
+    // toggleActions: 'restart pause reverse pause',
+    // scrub: true,
+    scrub: 1,
+    // markers: {
+    //   startColor: 'blue',
+    //   endColor: 'red',
+    //   fontSize: '50px',
+    //   indent: 200
+    // }
+    markers: true
+  }
 })
 
